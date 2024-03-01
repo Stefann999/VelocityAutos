fc8513df673163d8b782db80abc2ff822ba50c4d
Stefann999 <stefchog25@gmail.com>
2024-03-02 01:12:07 +0200

    Add Post service

---
+++ VelocityAutos.Services.Data/Interfaces/IPostService.cs
@@ -0,0 +1,11 @@
+﻿using VelocityAutos.Data.Models;
+using VelocityAutos.Web.ViewModels.Post;
+
+namespace VelocityAutos.Services.Data.Interfaces
+{
+    public interface IPostService
+    {
+        public Task CreateAsync(PostFormModel postFormModel, Car car, string currUserId);
+        //public Task<bool> IsUserPostOwnerById(string carId, string userId);
+    }
+}
---
+++ VelocityAutos.Services.Data/PostService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelocityAutos.Data;
+using VelocityAutos.Data.Models;
+using VelocityAutos.Services.Data.Interfaces;
+using VelocityAutos.Web.ViewModels.Post;
+
+namespace VelocityAutos.Services.Data
+{
+    public class PostService : IPostService
+    {
+        private readonly VelocityAutosDbContext dbContext;
+
+        public PostService(VelocityAutosDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task CreateAsync(PostFormModel postFormModel, Car car, string currUserId)
+        {
+           var post = new Post
+           {
+               CarId = car.Id,
+               Car = car,
+               SellerFirstName = postFormModel.FirstName,
+               SellerLastName = postFormModel.LastName,
+               SellerPhoneNumber = postFormModel.PhoneNumber,
+               SellerEmailAddress = postFormModel.EmailAddress,
+               SellerId = Guid.Parse(currUserId),
+               CreatedOn = DateTime.UtcNow,
+               UpdatedOn = null,
+               IsActive = true,
+           };
+
+            await this.dbContext.Posts.AddAsync(post);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+        //public async Task<bool> IsUserPostOwnerById(string carId, string userId)
+        //{
+        //    Car? car = await this.dbContext
+        //        .Cars
+        //        .AsNoTracking()
+        //        .FirstOrDefaultAsync(c => c.Id.ToString() == carId);
+
+        //    if (car == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    return car.Post.SellerId.ToString() == userId;
+        //}
+    }
+}
