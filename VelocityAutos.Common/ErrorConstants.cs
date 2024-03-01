0757e93b6bbc0b9a0bd74148ea088b0d68e1b181
Stefann999 <stefchog25@gmail.com>
2024-03-02 01:09:31 +0200

    Create ErrorConstants.cs

---
+++ VelocityAutos.Common/ErrorConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace VelocityAutos.Common
+{
+    public class ErrorConstants
+    {
+        public const string RequiredErrorMessage = "The {0} field is required.";
+        public const string StringLengthErrorMessage = "The field {0} must be at least {2} and at max {1} characters long.";
+        public const string InvalidEmailAddressMessage = "Invalid email address!";
+    }
+}
