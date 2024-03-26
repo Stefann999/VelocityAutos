f61c55871636c4d5b0a2e45141f81a0e7749e6eb
Stefann999 <stefchog25@gmail.com>
2024-03-26 23:08:11 +0200

    Added greeting to the user in navbar

---
+++ VelocityAutos.Services.Data/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VelocityAutos.Data;
+using VelocityAutos.Data.Models;
+using VelocityAutos.Services.Data.Interfaces;
+using VelocityAutos.Web.Infrastructure.Common;
+
+namespace VelocityAutos.Services.Data
+{
+    public class UserService : IUserService
+    {
+        private readonly VelocityAutosDbContext dbContext;
+        private readonly IRepository repository;
+
+        public UserService(VelocityAutosDbContext dbContext, IRepository repository)
+        {
+            this.dbContext = dbContext;
+            this.repository = repository;
+        }
+
+        public async Task<string> GetFullNameByEmailAddress(string emailAddress)
+        {
+            ApplicationUser? user = await repository.AllAsReadOnly<ApplicationUser>()
+                .Where(u => u.Email == emailAddress)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return string.Empty;
+            }
+
+            return $"{user.FirstName} {user.LastName}";
+        }
+    }
+}
