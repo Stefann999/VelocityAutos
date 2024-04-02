d548a6cf2786c51a80b8bb04392586fd15589fec
Stefann999 <stefchog25@gmail.com>
2024-04-02 18:24:41 +0300

    Update CarService.cs
    Remove auto mapper due to not showing types properly

--- VelocityAutos.Services.Data/CarService.cs
+++ VelocityAutos.Services.Data/CarService.cs
@@ -119,8 +119,26 @@ namespace VelocityAutos.Services.Data
         {
             var car = await repository.AllAsReadOnly<Car>()
                 .Where(c => c.Id.ToString() == carId)
-                .To<CarDetailsViewModel>()
-                .FirstOrDefaultAsync();
+				.Select(c => new CarDetailsViewModel
+				{
+					Id = c.Id.ToString(),
+					Make = c.Make,
+					Model = c.Model,
+					Price = c.Price,
+					Month = c.Month,
+					Year = c.Year,
+					Mileage = c.Mileage,
+					HorsePower = c.HorsePower,
+					FuelConsumption = c.FuelConsumption,
+					Color = c.Color,
+					Description = c.Description,
+					LocationCity = c.LocationCity,
+					LocationCountry = c.LocationCountry,
+					Category = c.Category.Name,
+					FuelType = c.FuelType.Name,
+					TransmissionType = c.TransmissionType.Name
+				})
+				.FirstOrDefaultAsync();
 
             return car;
         }
